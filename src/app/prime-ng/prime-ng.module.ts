81502b8a8009924878f8fcbb88fad0353478f5df
Luis Silva <luis.guerreros@elektra.com.mx>
2023-09-04 17:31:12 -0600

    first commit

---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,22 @@
+//import { MenuModule } from 'primeng/menu';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { FieldsetModule } from 'primeng/fieldset';
+import { MenubarModule } from 'primeng/menubar';
+import { NgModule } from '@angular/core';
+import { PanelModule } from 'primeng/panel';
+
+
+
+@NgModule({
+  declarations: [],
+  exports: [
+    //MenuModule
+    ButtonModule,
+    CardModule,
+    FieldsetModule,
+    MenubarModule,
+    PanelModule
+  ]
+})
+export class PrimeNGModule { }
---
+++ src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { PrimeIcons } from 'primeng/api';
+
+@Component({
+  selector: 'shared-menu',
+  templateUrl: './menu.component.html',
+  styles: [
+  ]
+})
+export class MenuComponent {
+  public items: MenuItem[]  = [];
+
+    constructor() {}
+
+    ngOnInit() {
+        this.items = [
+            {
+                label: 'Pipes de angular',
+                icon: 'pi pi-desktop',
+                items: [
+                    {
+                        label: 'Textos y fechas',
+                        icon: 'pi pi-align-left',
+                        routerLink: '/  '
+                    },
+                    {
+                        label: 'Números',
+                        icon: 'pi pi-dollar',
+                        routerLink: 'numbers'
+                    },
+                    {
+                        label: 'No comunes',
+                        icon: 'pi pi-globe',
+                        routerLink: 'uncommon'
+                    }
+                ]
+            },
+            {
+              label: 'Pipes personalizados',
+              icon: 'pi pi-cog'
+          }
+        ];
+    }
+
+}
